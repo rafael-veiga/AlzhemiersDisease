3090d8e405cc79ce18cb8940fc82254a84e55e2d
rafael-veiga <rafaelvalenteveiga@gmail.com>
2025-01-27 17:13:28 +0000

    create r docker

---
+++ create_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+repos="r1docker"
+
+if ! command -v docker &> /dev/null
+then
+    echo "Docker is not installed. Please install Docker to proceed."
+    exit 1
+fi
+
+docker build -t $repos ./dockers/$repos
+
